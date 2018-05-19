c5b7d9072a6ae2e6900e8868f9199458500aa6a7
sylvanase <362466464@qq.com>
2018-05-19 23:46:07 +0800

    base1

---
+++ src/components/vuex1.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>vuex base</h1>
+    <p>count from store: {{ $store.state.count }}</p>
+    <p><button @click="$store.commit('add')">add</button> <button @click="$store.commit('minus')">minus</button></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './components/vuex1' // 在此修改对应的组件
+//import router from './router/router10' // 在此处修改路由文件，查看不同效果
+import store from './store/store1'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+	store,
+	components: {
+		App
+	},
+	template: '<App/>'
+}).$mount('#app');
